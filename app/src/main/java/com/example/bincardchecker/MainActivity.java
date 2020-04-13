f51c6c7b29c96700b49572a887b04f2f078f31ae
cristian16b <cristian.budzicz@gmail.com>
2020-04-13 17:29:23 -0300

    Agregado boton buscar (falta implementar)

--- app/src/main/java/com/example/bincardchecker/MainActivity.java
+++ app/src/main/java/com/example/bincardchecker/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.bincardchecker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton nroTarjeta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nroTarjeta = (ImageButton) findViewById(R.id.botonBuscar);
+    }
+
+    public void buscarDatosTarjeta(View view){
+
     }
 }
